aa4181256ced1868e8aef7f5f9afd93dde004739
VictorSampaioDev <joaovictorsampaio.ti@gmail.com>
2025-03-12 16:51:52 -0300

    Initial Commit

---
+++ projeto E/projeto_EApp.swift
@@ -0,0 +1,17 @@
+//
+//  projeto_EApp.swift
+//  projeto E
+//
+//  Created by found on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct projeto_EApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
