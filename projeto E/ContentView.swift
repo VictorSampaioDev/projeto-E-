d81062ae199eed5d4b049f52d7ccb934c2dbf489
VictorSampaioDev <joaovictorsampaio.ti@gmail.com>
2025-03-13 10:52:49 -0300

    Update ContentView.swift

--- projeto E/ContentView.swift
+++ projeto E/ContentView.swift
@@ -7,18 +7,26 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct TelaInicialView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Image(systemName: "hand.wave.fill") // Ícone de mão acenando
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100) // Ajusta o tamanho
+                .foregroundColor(.blue) // Cor da mão
+            
+            Text("Olá, seja bem-vindo") // Texto abaixo da mão
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.black)
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct TelaInicialView_Previews: PreviewProvider {
+    static var previews: some View {
+        TelaInicialView()
+    }
 }
